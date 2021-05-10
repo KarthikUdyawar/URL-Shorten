09766b198e7c41e32dbb638ed96579d96fa3ec83
Karthik Udyawar <54676225+KarthikUdyawar@users.noreply.github.com>
2021-05-10 16:46:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 import pyshorteners
 from tkinter import *
 
+class App:
+    def __init__(self):
+        #! Initialization 
+        self.root = Tk()
+        self.root.title('URL shortener')
+        self.w, self.h = 300, 200
+        self.root.geometry("%dx%d" % (self.w, self.h))
+        self.root.bind("<Escape>", self.quit)
+        
+        
+        
+
+        #! Mainloop
+        self.root.mainloop()
+        
+    
+    def quit(self, event):
+        self.root.quit()
+
+if __name__ == '__main__':
+    app = App()  
+
