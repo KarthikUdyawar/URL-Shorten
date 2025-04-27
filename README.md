06f6cfb97d480fa47dd756abc38837f6f21d46c2
KarthikUdyawar <karthikajitudy@gmail.com>
2025-04-27 13:43:42 +0530

    feat: Added pre-commit

--- README.md
+++ README.md
@@ -1,65 +1,136 @@
 # URL-Shorten
+
+[![Python Version](https://img.shields.io/badge/Python-3.8+-blue.svg?style=flat&logo=python&logoColor=white)](https://www.python.org/)
 [![Powered by Poetry](https://img.shields.io/badge/Powered%20by-Poetry-060b55.svg?style=flat&logo=python&logoColor=white)](https://python-poetry.org/)
+[![Last Commit](https://img.shields.io/github/last-commit/KarthikUdyawar/URL-Shorten?style=flat)](https://github.com/KarthikUdyawar/URL-Shorten/commits/main)
+[![Repo Size](https://img.shields.io/github/repo-size/KarthikUdyawar/URL-Shorten?style=flat)](https://github.com/KarthikUdyawar/URL-Shorten)
+[![Open Issues](https://img.shields.io/github/issues/KarthikUdyawar/URL-Shorten?style=flat)](https://github.com/KarthikUdyawar/URL-Shorten/issues)
+[![Pull Requests](https://img.shields.io/github/issues-pr/KarthikUdyawar/URL-Shorten?style=flat)](https://github.com/KarthikUdyawar/URL-Shorten/pulls)
+[![License](https://img.shields.io/github/license/KarthikUdyawar/URL-Shorten?style=flat)](https://github.com/KarthikUdyawar/URL-Shorten/blob/main/LICENSE)
 
-A simple and lightweight URL shortener built with Python.
 
-## Screenshot
-![Screenshot of the app](https://github.com/KarthikUdyawar/URL-Shorten/blob/main/images/Screenshot.png)
+<br />
+<p align="center">
+  <a href="https://github.com/KarthikUdyawar/URL-Shorten">
+    <img src="images/icon.png" alt="Logo" width="80" height="80">
+  </a>
 
-## Features
-- Shorten long URLs easily
-- Clean and minimal UI
-- Easy to run locally
+  <h3 align="center">URL-Shorten</h3>
 
-## Requirements
+  <p align="center">
+    A simple and lightweight URL shortener built with Python.
+    <br />
+    <a href="https://github.com/KarthikUdyawar/URL-Shorten"><strong>Explore the docs »</strong></a>
+    <br />
+    <br />
+    <a href="#usage">View Demo</a>
+    ·
+    <a href="https://github.com/KarthikUdyawar/URL-Shorten/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/KarthikUdyawar/URL-Shorten/issues">Request Feature</a>
+  </p>
+</p>
 
-- Python 3.8+
+---
+
+## 📋 Table of Contents
+
+- [URL-Shorten](#url-shorten)
+  - [📋 Table of Contents](#-table-of-contents)
+  - [🛠 About The Project](#-about-the-project)
+  - [🚀 Built With](#-built-with)
+  - [🏁 Getting Started](#-getting-started)
+    - [Prerequisites](#prerequisites)
+    - [Installation](#installation)
+  - [📚 Usage](#-usage)
+  - [🤝 Contributing](#-contributing)
+  - [🪪 License](#-license)
+  - [📬 Contact](#-contact)
+  - [🙏 Acknowledgements](#-acknowledgements)
+
+---
+
+## 🛠 About The Project
+
+![Screenshot](https://github.com/KarthikUdyawar/URL-Shorten/blob/main/images/Screenshot.png)
+
+**URL-Shorten** allows users to easily shorten long URLs with a clean and minimal UI.
+Focus is on simplicity, local usage, and zero-dependency outside Python and Poetry.
+
+---
+
+## 🚀 Built With
+
+- [Python 3.8+](https://www.python.org/)
+- [Poetry](https://python-poetry.org/)
+
+---
+
+## 🏁 Getting Started
+
+### Prerequisites
+
+- Python 3.8 or newer
 - Poetry
 
-> You can install Poetry by following the [official guide](https://python-poetry.org/docs/#installation).
+### Installation
 
-## Project Structure
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/KarthikUdyawar/URL-Shorten
+   cd URL-Shorten
+   ```
+2. Install dependencies:
+   ```bash
+   poetry install
+   poetry env use python3
+   ```
 
-```
-.
-├── LICENSE
-├── README.md
-├── images
-│   ├── Screenshot.png
-│   └── icon.png
-├── main.py
-├── poetry.lock
-├── pyproject.toml
-└── test
-    └── test.py
-```
+---
 
-## Setup & Installation
+## 📚 Usage
 
-Make sure you have the latest version of **Python** and **Poetry** installed.
-
-Clone the repository:
-
-```bash
-git clone https://github.com/KarthikUdyawar/URL-Shorten
-cd URL-Shorten
-```
-
-Install dependencies:
-
-```bash
-poetry install
-poetry env activate
-```
-
-## Running the App
-
-Start the application:
+Run the application:
 
 ```bash
 poetry run python main.py
 ```
 
-## License
+Shorten your URLs easily through the UI!
 
-This project is licensed under the [MIT License](LICENSE).
+---
+
+## 🤝 Contributing
+
+Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+1. Fork the Project
+2. Create your Feature Branch (git checkout -b feat/AmazingFeature)
+3. Commit your Changes (git commit -m 'Add some AmazingFeature')
+4. Push to the Branch (git push origin feat/AmazingFeature)
+5. Open a Pull Request
+
+---
+
+## 🪪 License
+
+Distributed under the [MIT License](LICENSE).
+
+---
+
+## 📬 Contact
+
+Karthik Udyawar – [LinkedIn](https://www.linkedin.com/in/karthik-udyawar-0489991b3) – karthikajitudy@gmail.com
+
+
+Project Link: [https://github.com/KarthikUdyawar/URL-Shorten](https://github.com/KarthikUdyawar/URL-Shorten)
+
+---
+
+## 🙏 Acknowledgements
+
+- [Poetry](https://python-poetry.org/)
+- [Shield.io Badges](https://shields.io)
+- [Markdown Guide](https://www.markdownguide.org/)
+
+---
--- main.py
+++ main.py
@@ -1,56 +1,108 @@
-import pyshorteners as ps
-from tkinter import *
+"""URL Shortener App using Tkinter and Pyshorteners."""
+
+from tkinter import Button, Entry, Label, PhotoImage, StringVar, Tk
+
 import pyperclip
+import pyshorteners as ps
+
 
 class App:
+    """A simple URL Shortener application using Tkinter and pyshorteners."""
+
     def __init__(self):
-        
+        """Initializes the main window and application components."""
         self.root = Tk()
-        self.root.title('URL shortener')
+        self.root.title("URL Shortener")
         self.w, self.h = 500, 120
-        self.root.geometry("%dx%d" % (self.w, self.h))
+        self.root.geometry(f"{self.w}x{self.h}")
         self.root.bind("<Escape>", self.quit)
-        self.root.resizable(False,False)
-        self.icon = PhotoImage(file = 'images/icon.png')
+        self.root.resizable(False, False)
+
+        # Set the application icon
+        self.icon = PhotoImage(file="images/icon.png")
         self.root.iconphoto(False, self.icon)
-        
+
+        # Variables
         self.url_var = StringVar()
         self.error_var = StringVar()
-        
-        self.url_label = Label(self.root, text = "Enter link to shorten: " , font=('calibre',10,'normal')).place(x = 150,y = 20)  
-        self.url_entry = Entry(self.root,textvariable = self.url_var, font=('calibre',10,'normal'), width=50).place(x = 40,y = 40)  
-        self.url_btn =Button(self.root, text ="Shorten", command = self.shorten, bg='yellow').place(x = 410,y = 35) 
-        self.copy_btn =Button(self.root, text ="Copy", command = self.copy_to_clipboard).place(x = 100,y = 70) 
-        self.paste_btn =Button(self.root, text ="Paste", command = self.paste_to_clipboard).place(x = 200,y = 70) 
-        self.clear_btn =Button(self.root, text ="Clear", command = self.clear).place(x = 300,y = 70) 
-        self.error_label = Label(self.root, text = "", font=('calibre',10,'normal'))
-        self.error_label.place(x = 150,y = 100)  
-        
+
+        # Layout
+        Label(
+            self.root,
+            text="Enter link to shorten:",
+            font=("calibre", 10, "normal"),
+        ).place(x=150, y=20)
+        Entry(
+            self.root,
+            textvariable=self.url_var,
+            font=("calibre", 10, "normal"),
+            width=50,
+        ).place(x=40, y=40)
+        Button(
+            self.root, text="Shorten", command=self.shorten, bg="yellow"
+        ).place(x=410, y=35)
+        Button(self.root, text="Copy", command=self.copy_to_clipboard).place(
+            x=100, y=70
+        )
+        Button(self.root, text="Paste", command=self.paste_to_clipboard).place(
+            x=200, y=70
+        )
+        Button(self.root, text="Clear", command=self.clear).place(x=300, y=70)
+
+        self.response_label = Label(
+            self.root, text="", font=("calibre", 10, "normal")
+        )
+        self.response_label.place(x=150, y=100)
+
         self.root.mainloop()
-        
+
     def quit(self, event):
+        """Exits the application when the Escape key is pressed."""
         self.root.quit()
-    
+
     def shorten(self):
+        """Shortens the URL entered by the user and updates the label with success \
+        or error message."""
         try:
             url = str(self.url_var.get())
+            if not url:
+                raise ValueError("Please enter a URL.")
             shorten = ps.Shortener()
             shorten_url = shorten.tinyurl.short(url)
-            self.url_var.set(str(shorten_url))
-            self.error_label.config(text='Successful shorten',fg='green')
+            self.url_var.set(shorten_url)
+            self.response_label.config(
+                text="Successfully shortened!", fg="green"
+            )
+        except ValueError as ve:
+            self.response_label.config(text=str(ve), fg="red")
         except ps.exceptions.BadURLException:
-            self.error_label.config(text='URL is not valid',fg='red')
+            self.response_label.config(text="URL is not valid", fg="red")
         except ps.exceptions.ShorteningErrorException:
-            self.error_label.config(text='It is already shorten',fg='red')
-    
-    def copy_to_clipboard(self):
-        pyperclip.copy(self.url_var.get())
-        
-    def paste_to_clipboard(self):
-        self.url_var.set(pyperclip.waitForPaste()) 
-        
-    def clear(self):
-        self.url_var.set("") 
+            self.response_label.config(
+                text="Error shortening the URL", fg="red"
+            )
 
-if __name__ == '__main__':
-    app = App()  
+    def copy_to_clipboard(self):
+        """Copies the shortened URL to the clipboard."""
+        if self.url_var.get():
+            pyperclip.copy(self.url_var.get())
+            self.response_label.config(
+                text="URL copied to clipboard", fg="green"
+            )
+        else:
+            self.response_label.config(text="No URL to copy", fg="red")
+
+    def paste_to_clipboard(self):
+        """Pastes a URL from the clipboard to the URL entry field."""
+        paste_data = pyperclip.paste()
+        self.url_var.set(paste_data)
+        self.response_label.config(text="URL pasted", fg="green")
+
+    def clear(self):
+        """Clears the URL entry field and the response label."""
+        self.url_var.set("")
+        self.response_label.config(text="", fg="black")
+
+
+if __name__ == "__main__":
+    app = App()
